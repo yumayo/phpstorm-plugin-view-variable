c58261dcbe916a8bfad933929da34076fd665bed
y-mayo-dev <y.mayo.dev@gmail.com>
2025-05-18 01:16:04 +0900

    PHPの変数はReferenceProviderで反応しなさそうなため、全てに反応するProviderを用意しました。

---
+++ src/main/java/com/sample/phpstormpluginviewvariable/AllPsiElementReferenceProvider.java
@@ -0,0 +1,19 @@
+package com.sample.phpstormpluginviewvariable;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.PsiReferenceProvider;
+import com.intellij.util.ProcessingContext;
+import org.jetbrains.annotations.NotNull;
+
+public class AllPsiElementReferenceProvider extends PsiReferenceProvider {
+
+    @Override
+    public PsiReference @NotNull [] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+        Log.info("Element type: " + element.getClass().getSimpleName());
+        Log.info("Element text: " + element.getText());
+        Log.info("Element location: " + element.getContainingFile().getName() + ":" + element.getTextOffset());
+        
+        return PsiReference.EMPTY_ARRAY;
+    }
+} 
