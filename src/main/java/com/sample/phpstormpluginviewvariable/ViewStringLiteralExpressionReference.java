a8161a983ce2a037a4954110076244234ea89efd
y-mayo-dev <y.mayo.dev@gmail.com>
2025-05-17 22:22:59 +0900

    コントローラーファイルからビューファイルへのナビゲーション機能を追加しました。

--- src/main/java/com/sample/phpstormpluginviewvariable/ViewStringLiteralExpressionReference.java
+++ src/main/java/com/sample/phpstormpluginviewvariable/ViewStringLiteralExpressionReference.java
@@ -1,6 +1,7 @@
 package com.sample.phpstormpluginviewvariable;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
@@ -52,43 +53,132 @@ public class ViewStringLiteralExpressionReference extends PsiReferenceBase<Strin
             Log.info("Not inside a method");
         }
 
-        // コントローラーのクラス名からビューファイルのパスを推測
-        PhpClass controllerClass = PsiTreeUtil.getParentOfType(myElement, PhpClass.class);
-        if (controllerClass == null) {
-            Log.info("parentClass not found");
+        // 現在のファイルがコントローラーファイルかビューファイルかを判定
+        PsiFile currentFile = myElement.getContainingFile();
+        if (currentFile == null) {
             return null;
         }
 
-        String className = controllerClass.getName();
-        Log.info("controllerClass: " + className);
+        String filePath = currentFile.getVirtualFile().getPath();
+        boolean isControllerFile = filePath.contains("/Controller/");
+        boolean isViewFile = filePath.contains("/View/");
 
-        // "Controller"サフィックスを削除してビュー名を取得
-        String viewName = className.replace("Controller.php", "").toLowerCase();
+        if (isControllerFile) {
+            // コントローラーファイルからビューファイルへのジャンプ
+            return resolveToViewFile(currentFile, varName, project);
+        } else if (isViewFile) {
+            // ビューファイルからコントローラーファイルへのジャンプ
+            return resolveToControllerFile(currentFile, varName, project);
+        }
 
-        Log.info("viewName: " + viewName);
+        return null;
+    }
 
-        // ビューファイルを検索
-        Collection<VirtualFile> viewFiles = FilenameIndex.getVirtualFilesByName(viewName + ".php", GlobalSearchScope.projectScope(project));
-        Log.info("viewFiles found: " + viewFiles.size());
+    /**
+     * コントローラーファイルからビューファイルへのジャンプを実装
+     */
+    private PsiElement resolveToViewFile(PsiFile controllerFile, String varName, Project project) {
+        // コントローラーファイルのパス例: /modules/Cli/Controller/Debug/TestController.php
+        String controllerPath = controllerFile.getVirtualFile().getPath();
+        String fileName = controllerFile.getVirtualFile().getName(); // TestController.php
 
-        // 各ビューファイルから変数を検索
-        for (VirtualFile viewFile : viewFiles) {
-            PsiFile psiFile = PsiManager.getInstance(project).findFile(viewFile);
-            if (psiFile == null) {
-                continue;
-            }
-
-            Collection<Variable> variables = PsiTreeUtil.findChildrenOfType(psiFile, Variable.class);
-            Log.info("variables in file " + viewFile.getName() + ": " + variables.size());
-            for (Variable variable : variables) {
-                if (varName.equals(variable.getName())) {
-                    Log.info("MATCHED variable (usage): " + variable.getName());
-                    return variable;
-                }
+        // サブディレクトリとコントローラー名を取得
+        String[] split = controllerPath.split("/Controller/");
+        String subDir = "";
+        String controllerClassName = fileName.replace("Controller.php", ""); // Test
+        if (split.length == 2) {
+            String afterController = split[1]; // 例: Debug/TestController.php
+            int slashIdx = afterController.lastIndexOf("/");
+            if (slashIdx > 0) {
+                subDir = afterController.substring(0, slashIdx); // 例: Debug
             }
         }
 
-        Log.info("No match found");
+        // setVarの呼び出し元メソッド（アクション名）を取得
+        Method containingMethod = PsiTreeUtil.getParentOfType(myElement, Method.class);
+        String actionName = "index"; // デフォルト
+        if (containingMethod != null) {
+            String methodName = containingMethod.getName();
+            if (methodName.endsWith("Action")) {
+                actionName = methodName.substring(0, methodName.length() - "Action".length());
+            } else {
+                actionName = methodName;
+            }
+        }
+
+        // Viewファイルのパスを組み立て
+        String viewPath = controllerPath.substring(0, controllerPath.indexOf("/Controller/")) + "/View/";
+        if (!subDir.isEmpty()) {
+            viewPath += subDir + "/";
+        }
+        viewPath += controllerClassName.toLowerCase() + "/" + actionName + ".php";
+
+        VirtualFile viewVirtualFile = LocalFileSystem.getInstance().findFileByPath(viewPath);
+        if (viewVirtualFile == null) {
+            Log.info("View file not found: " + viewPath);
+            return null;
+        }
+
+        PsiFile viewFile = PsiManager.getInstance(project).findFile(viewVirtualFile);
+        if (viewFile == null) {
+            return null;
+        }
+
+        // ビューファイル内の変数を検索
+        Collection<Variable> variables = PsiTreeUtil.findChildrenOfType(viewFile, Variable.class);
+        for (Variable variable : variables) {
+            if (varName.equals(variable.getName())) {
+                Log.info("Found variable in view file: " + variable.getName());
+                return variable;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * ビューファイルからコントローラーファイルへのジャンプを実装
+     */
+    private PsiElement resolveToControllerFile(PsiFile viewFile, String varName, Project project) {
+        // ビューファイルのパスからコントローラーファイルのパスを推測
+        String viewPath = viewFile.getVirtualFile().getPath();
+        String controllerPath = viewPath.replace("/View/", "/Controller/");
+        controllerPath = controllerPath.replace(".php", "Controller.php");
+
+        VirtualFile controllerVirtualFile = LocalFileSystem.getInstance().findFileByPath(controllerPath);
+        if (controllerVirtualFile == null) {
+            Log.info("Controller file not found: " + controllerPath);
+            return null;
+        }
+
+        PsiFile controllerFile = PsiManager.getInstance(project).findFile(controllerVirtualFile);
+        if (controllerFile == null) {
+            return null;
+        }
+
+        // コントローラーファイル内のsetVar呼び出しを検索
+        Collection<MethodReference> methodRefs = PsiTreeUtil.findChildrenOfType(controllerFile, MethodReference.class);
+        for (MethodReference methodRef : methodRefs) {
+            if (!"setVar".equals(methodRef.getName())) {
+                continue;
+            }
+
+            PsiElement[] args = methodRef.getParameters();
+            if (args.length < 2) {
+                continue;
+            }
+
+            if (!(args[0] instanceof StringLiteralExpression)) {
+                continue;
+            }
+
+            StringLiteralExpression keyArg = (StringLiteralExpression) args[0];
+            if (varName.equals(keyArg.getContents())) {
+                Log.info("Found setVar call in controller: " + methodRef.getName());
+                return methodRef;
+            }
+        }
+
         return null;
     }
 
